d2e6faadf6680613891e69bbc9efd36b3ce8fb60
Tocny <marcoarubior@ciencias.unam.mx>
2024-09-09 14:44:27 -0600

    Empezamos a implementar decorator.

--- practica/src/main/java/mx/unam/ciencias/modelado/practica2/decorator/AleronDeportivo.java
+++ practica/src/main/java/mx/unam/ciencias/modelado/practica2/decorator/AleronDeportivo.java
@@ -1,1 +1,23 @@
 package mx.unam.ciencias.modelado.practica2.decorator;
+
+import mx.unam.ciencias.modelado.practica2.template.Vehiculo;
+
+/**Aditamento: Alerón Deportivo. */
+public class AleronDeportivo extends Aditamento{
+
+    /**
+     * Constructor de la clase.
+     * @param vehiculo la instancia de Vehiculo para un atributo de la clase padre.
+     */
+    public AleronDeportivo(Vehiculo vehiculo){
+        super(vehiculo);
+    }
+
+    /**
+     * Implementación concreta del método descripción.
+     * @return la descrición del vehiculo con la cadena ", alerón deportivo"
+     */
+    @Override public String descripcion(){
+        return super.vehiculo.descripcion() + ", alerón deportivo";
+    }
+}
