22ba6245b4ab686dea918a141e172eaafb494901
goldenemilly <adamleon@ciencias.unam.mx>
2024-09-10 08:10:00 -0600

    String no info.

--- practica/src/main/java/mx/unam/ciencias/modelado/practica2/template/Vehiculo4x4.java
+++ practica/src/main/java/mx/unam/ciencias/modelado/practica2/template/Vehiculo4x4.java
@@ -1,1 +1,13 @@
 package mx.unam.ciencias.modelado.practica2.template;
+class Vehiculo4x4 extends Vehiculo{
+// Constructor
+public Vehiculo4x4(EstadoVehiculo estado) {
+    super(estado);
+    }
+public String sigueRuta(){
+return "";//¿Quien me da esa info?
+}
+public String alertaCombustible(){
+return "";//¿Quien me da esa info?
+}
+}
