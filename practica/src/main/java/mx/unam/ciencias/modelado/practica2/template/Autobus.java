c423876c2304b0ac213c6dbcd856e6e29cab5ff3
goldenemilly <adamleon@ciencias.unam.mx>
2024-09-10 08:07:17 -0600

    String no info .

--- practica/src/main/java/mx/unam/ciencias/modelado/practica2/template/Autobus.java
+++ practica/src/main/java/mx/unam/ciencias/modelado/practica2/template/Autobus.java
@@ -1,1 +1,13 @@
 package mx.unam.ciencias.modelado.practica2.template;
+class Autobus extends Vehiculo{
+// Constructor
+public Autobus(EstadoVehiculo estado) {
+    super(estado);
+    }
+public String sigueRuta(){
+return "";//¿Quien me da esa info?
+}
+public String alertaCombustible(){
+return "";//¿Quien me da esa info?
+}
+}
