c0144c8e480455139ed3f8541c846ebbc5a71762
goldenemilly <adamleon@ciencias.unam.mx>
2024-09-10 08:08:50 -0600

    String no info.

--- practica/src/main/java/mx/unam/ciencias/modelado/practica2/template/Scooter.java
+++ practica/src/main/java/mx/unam/ciencias/modelado/practica2/template/Scooter.java
@@ -1,1 +1,13 @@
 package mx.unam.ciencias.modelado.practica2.template;
+class Scooter extends Vehiculo{
+// Constructor
+public Scooter(EstadoVehiculo estado) {
+    super(estado);
+    }
+public String sigueRuta(){
+return "";//¿Quien me da esa info?
+}
+public String alertaCombustible(){
+return "";//¿Quien me da esa info?
+}
+}
