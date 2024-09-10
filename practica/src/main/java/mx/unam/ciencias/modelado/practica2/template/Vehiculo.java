0fb6d33bfebcbac30b48e7b214b9f9740a23fb62
goldenemilly <adamleon@ciencias.unam.mx>
2024-09-10 07:51:43 -0600

    Primera implementación, falta de desarrollo del objeto.

--- practica/src/main/java/mx/unam/ciencias/modelado/practica2/template/Vehiculo.java
+++ practica/src/main/java/mx/unam/ciencias/modelado/practica2/template/Vehiculo.java
@@ -1,1 +1,23 @@
 package mx.unam.ciencias.modelado.practica2.template;
+abstract class Vehiculo {
+protected EstadoVehiculo estadoActual;
+
+// Constructor
+public Vehiculo(EstadoVehiculo estado) {
+this.estadoActual = estado;
+
+}
+
+    protected abstract void ejecutaViaje();
+    protected abstract String esperaViaje();
+    protected abstract String verificaPasajero();
+    protected abstract String empiezaViaje();
+    protected abstract String finalizaViaje();
+    protected abstract String sigueRuta();
+    protected abstract String alertaCombustible();
+    protected abstract String descripcion();
+
+    public void setEstado(EstadoVehiculo estado) {
+        this.estadoActual = estado;
+    }
+}
