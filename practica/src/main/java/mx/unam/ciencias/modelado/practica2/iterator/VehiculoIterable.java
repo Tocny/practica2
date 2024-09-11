353798a66fc859bce230bb69002278c2c7ddb57f
Tocny <marcoarubior@ciencias.unam.mx>
2024-09-11 00:07:08 -0600

    Cambios en la implementacion de iterator.

---
+++ practica/src/main/java/mx/unam/ciencias/modelado/practica2/iterator/VehiculoIterable.java
@@ -0,0 +1,28 @@
+package mx.unam.ciencias.modelado.practica2.iterator;
+
+import java.lang.Iterable;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Clase que implementa la interfaz Iterable de la clase java.lang
+ * para instancias de la clase Vehiculo.
+ */
+public class VehiculoIterable implements Iterable<Vehiculo>{
+    /**Lista de vehículos. */
+    private List<Vehiculo> vehiculos;
+
+    /**Constructor de la clase, inicializa la lista de vehículos. */
+    public VehiculoIterable(){
+        vehiculos = new ArrayList<>();
+    }
+    
+    /**
+     * Método que regresa un iterador.
+     * @return una instancia de VehiculoIterator
+     */
+    public Iterator<Vehiculo> iterator(){
+        return new VehiculoIterator(vehiculos);
+    }
+}
