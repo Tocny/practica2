2c33a0ccd46374b34d8355806b61b75638da41c6
Tocny <marcoarubior@ciencias.unam.mx>
2024-09-13 02:32:07 -0600

    Documentacion de la clase Menu

--- practica/src/main/java/mx/unam/ciencias/modelado/practica2/simulaciones/Menu.java
+++ practica/src/main/java/mx/unam/ciencias/modelado/practica2/simulaciones/Menu.java
@@ -2,13 +2,21 @@ package mx.unam.ciencias.modelado.practica2.simulaciones;
 
 import mx.unam.ciencias.modelado.practica2.template.*;
 
+/**
+ * Clase general de los menús del usuario para escoger y tunear vehiculos
+ * así como manejar el viaje a realizar.
+ */
 public class Menu{
-    Vehiculo vehiculo = new VehiculoNulo();
-    VehiculoManager selectorVehiculos;
-    AditamentosManager selectorAditamentos;
-    DestinosManager selectorDestinos;
-    Vehiculo modelo;
-    Vehiculo vehiculoUsuario;
+    /**Instancia de VehiculoManager. */
+    private VehiculoManager selectorVehiculos;
+    /**Instancia de AditamentosManager. */
+    private AditamentosManager selectorAditamentos;
+    /**Instancia de DestinosManager. */
+    private DestinosManager selectorDestinos;
+    /**Modelo base del vehiculo del usuario. */
+    private Vehiculo modelo;
+    /**Vehiculo tuneado por el usuario. */
+    private Vehiculo vehiculoUsuario;
 
     /**Método general de los menús del proyecto. */
     public void ejecutaMenu(){
